57ad216bc32bd1cc6593b63fc717fd22e38bf4a2
Vango121 <barski93@gmail.com>
2020-08-09 14:30:34 +0200

    init

---
+++ app/src/androidTest/java/com/marcel/mycompany/WorkersFragmentTest.kt
@@ -0,0 +1,34 @@
+package com.marcel.mycompany
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.junit.Test
+
+
+@RunWith(AndroidJUnit4::class)
+class WorkersFragmentTest{
+    @Test
+    fun test_areImageViewsDisplayed() {
+        val scenario = launchFragmentInContainer<WorkersFragment>()
+        onView(withId(R.id.imageView2)).check(matches(isDisplayed())) // add workers
+        onView(withId(R.id.imageView3)).check(matches(isDisplayed())) //remove workers
+        onView(withId(R.id.imageView4)).check(matches(isDisplayed())) // edit contract
+        onView(withId(R.id.imageView5)).check(matches(isDisplayed())) // wallet icon
+    }
+
+    @Test
+    fun test_areTextsDisplayed() {
+        val scenario = launchFragmentInContainer<WorkersFragment>()
+        onView(withId(R.id.textView)).check(matches(withText(R.string.manage_workers))) // manage workers text
+        onView(withId(R.id.textViewEdit)).check(matches(withText(R.string.edit_contracts_workers))) // edit_contracts_workers text
+        onView(withId(R.id.textViewHours)).check(matches(withText(R.string.working_hours))) // working hours string
+        onView(withId(R.id.textViewPay)).check(matches(withText(R.string.sum_amount_workers))) // sum of payment
+
+    }
+}
